782109177af472575dfee2add866b84e846d11d2
Myproject <tahirbinkhadim@gmail.com>
2024-09-25 00:27:41 +0500

    Add existing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Button } from "@/components/ui/button"
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className="font-bold text-3xl text-red-600">hello word</h1>
+     <Button variant='outline'>Click me</Button>
+    </>
+  )
+}
+
+export default App
