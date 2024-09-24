8fb9f5b12a6fd76126dff8b8926f744c3002d51f
Myproject <tahirbinkhadim@gmail.com>
2024-09-25 00:34:42 +0500

    add client

--- src/app.js
+++ src/app.js
@@ -11,6 +11,14 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
+    methods:['GET','POST','DELETE','PUT'] ,
+    allowedHeaders:[
+      "Content-Type",
+      "Authorization",
+      "Cache-control",
+      "Expires",
+      "Pragma"
+    ],
     credentials: true,
   })
 );
